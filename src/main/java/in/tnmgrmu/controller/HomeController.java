978620b82d133a74db26400127b6509fd65e8d29
Senthil G <gdr.senthil@gmail.com>
2017-04-07 11:34:56 +0530

    mgrcourseapp springboot setup with welcome page

---
+++ src/main/java/in/tnmgrmu/controller/HomeController.java
@@ -0,0 +1,14 @@
+package in.tnmgrmu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping
+	public String index(){
+		System.out.println("HomeController ->index");
+		return "index";
+	}
+}
